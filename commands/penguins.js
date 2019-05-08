685757f3ecccabb1f4a90c145878d7681c10ff10
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-08 16:40:07 +0100

    Update penguins.js

--- commands/penguins.js
+++ commands/penguins.js
@@ -1,18 +1,20 @@
-const Discord = module.require("discord.js")
-const randomPenguin = require("random-penguin")
-const moment = require("moment")
-module.exports.run = async (bot, message) => {
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`penguin`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+const Discord = require("discord.js")
+const moment = require("moment");
+const superagent = require('snekfetch');
+module.exports.run = async (bot, message, args) => {
+    let {body} = await superagent
+    .get(`https://animals.anidiots.guide/penguin`);
+    //var penguin = penguins[Math.floor(Math.random() * penguins.length)]
+    var pengname = name[Math.floor(Math.random() * name.length)]
+ //var pengdes = description[Math.floor(Math.random() * description.length)]
+   let embed = new Discord.RichEmbed()
+   .setTitle("Here is one of my people")
+   .setDescription(`This is ${pengname}`)
+   .setImage(body.link)
+   .setColor(12812876)
+   .setFooter(`Requested by ${message.author.username} | ${moment(new Date).format('DD/MM/YYYY [at] hh:mm:ss a')}`);
 
-    let msg = await message.channel.send("Searching for Penguins");
-    if(!randomPenguin) return message.channel.send("Could not locate any Penguins :frowning:")
-        randomPenguin().then(url => {
-           
-            msg.channel.send(`${url}`)
-        })
+   message.channel.send({embed});
 }
 
 module.exports.help = {
