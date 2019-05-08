b9be09bac98827b484b37d7bfccbfb1a14da7c67
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-08 16:26:32 +0100

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,7 @@
-{
-"GreenLogin": process.env.token,
-"GreenTestingLogin": "--token here--",
-"prefix": "g!",
-"greenOwner": "188861825100677120",
-"botDev": "272442568275525634",
-"ownerDev": ["188861825100677120", "272442568275525634"]
-}
+ module.exports = {
+      GreenLogin: process.env.token,
+      prefix: 'g!',
+      greenOwner: "188861825100677120",
+      botDev: "272442568275525634",
+      ownerDev: ["188861825100677120", "272442568275525634"]
+    }
