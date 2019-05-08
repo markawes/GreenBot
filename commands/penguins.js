9b4b3acdcaafbdbb2676b25a49b9053363bd745c
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-08 16:41:28 +0100

    Update penguins.js

--- commands/penguins.js
+++ commands/penguins.js
@@ -4,12 +4,9 @@ const superagent = require('snekfetch');
 module.exports.run = async (bot, message, args) => {
     let {body} = await superagent
     .get(`https://animals.anidiots.guide/penguin`);
-    //var penguin = penguins[Math.floor(Math.random() * penguins.length)]
-    var pengname = name[Math.floor(Math.random() * name.length)]
- //var pengdes = description[Math.floor(Math.random() * description.length)]
    let embed = new Discord.RichEmbed()
-   .setTitle("Here is one of my people")
-   .setDescription(`This is ${pengname}`)
+   .setTitle("Here is a penguin")
+   .setDescription(`All yours ${message.author.username}`)
    .setImage(body.link)
    .setColor(12812876)
    .setFooter(`Requested by ${message.author.username} | ${moment(new Date).format('DD/MM/YYYY [at] hh:mm:ss a')}`);
