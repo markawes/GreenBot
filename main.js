0b3364e0e93b02c5733a2477c17aa15d1bb5fdee
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-08 17:14:19 +0100

    Green wanted cookies

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const fs = require("fs"); //loads module to read files
 const bot = new Discord.Client({disableEveryone: true}) //the client and prevent bot from pinging everyone
 const botConfig = require("./botConfig.js"); //the config file
 const prefix = botConfig.prefix; //the prefix from the config file
+const sexy = require("chalk") //define the sexyness
+const banished = require("./classes/noGreenBot4u.json")
 bot.commands = new Discord.Collection(); //for commands
 bot.ratelimits = new Discord.Collection(); // create collection of rate limit
 
@@ -11,11 +13,11 @@ fs.readdir("./commands/", (err, files) => {
 
 	let jsfiles = files.filter(f => f.split(".").pop() === "js");
 	if(jsfiles.length <= 0) {
-		console.log("No commands to load!");
+		console.log(sexy.red("No commands to load!"));
 		return;
 	}
 
-	console.log(`Loading ${jsfiles.length} commands!`);
+	console.log(sexy.white.bgBlue.bold(`Loading ${jsfiles.length} commands!`));
 
 	jsfiles.forEach((f, i) => {
 		let props = require(`./commands/${f}`);
@@ -25,41 +27,139 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 bot.on('ready', () => {
-  console.log(`Greensanoob is online! and logged in as ${bot.user.username}`); //if this ever shows anything else but GreenBot then shut it down and tell me
-  bot.user.setPresence({ status: 'online', game: { name: 'Discord' } });
+  //console.log(sexy.white.bgGreen.bold(`Greensanoob is online! and logged in as ${bot.user.username}`)); //if this ever shows anything else but GreenBot then shut it down and tell me
+  console.log(sexy.white.bgGreen.bold(`
+  Bot Account: ${bot.user.tag}
+  Bot ID: ${bot.user.id}
+  Server Count: ${bot.guilds.size}
+  Emoji Count: ${bot.emojis.size}
+  Channel Count: ${bot.channels.size}
+  User Count: ${bot.users.size}
+  I am ready!
+  `)); 
+  setInterval(() => {  
+	bot.user.setPresence({
+		status: "online",
+		game: {
+		name: "g!help | For Help Join My Discord Server https://discord.gg/JhkyCSy | Come Follow My Stream",
+		type: "STREAMING",
+		url: "https://www.twitch.tv/greensapenguin"
+		}
+		}) }, 900000)
 });
 
 //sid = 389472576235372565
 
+bot.on("guildCreate", async guild => {
+	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+	 //BEGIN NEW GUILD ADD
+	 let serverSize = guild.memberCount;
+	 let botCount = guild.members.filter(m => m.user.bot).size;
+	 let humanCount = serverSize - botCount;
+	 let verifLevels = ["None", "Low\nmust have verified\nemail on account", "Medium - must be registered on Discord for longer than 5 minutes", "High -  (╯°□°）╯︵ ┻━┻ - must be a member of the server for longer than 10 minutes", "Very High - ┻━┻ミヽ(ಠ益ಠ)ﾉ彡┻━┻ - must have a verified phone number"];
+	
+ 	const newserverembed = new Discord.RichEmbed()
+		 .setColor(`0x008000`)
+		 .setAuthor(`Owner: ${guild.owner.user.tag}`, guild.owner.user.displayAvatarURL)
+		 .setFooter(`Guild Name: ${guild.name} ID: ${guild.id}`, guild.iconURL)
+		 .setThumbnail(guild.iconURL ? guild.iconURL : guild.owner.user.displayAvatarURL)
+		 .setTimestamp()
+		 .setTitle(`Server Joined`)
+		 .addField(`Server Name`, guild.name, true)
+		 .addField(`Server ID`, guild.id, true)
+		 .addField(`Server Owner`, guild.owner.user.tag, true)
+		 .addField(`Server Owner ID`, guild.ownerID, true)
+		 .addField(`Server Region`, guild.region, true)
+		 .addField(`Verification Level`, verifLevels[guild.verificationLevel], true)
+		 .addField(`Total Members`, serverSize, true)
+		 .addField(`Total Bots`, botCount, true)
+		 .addField(`Total Humans`, humanCount, true)
+		 .addField(`Emoji Count`, guild.emojis.size, true)
+		 .addField(`Role Count`, guild.roles.size, true)
+		 .addField(`Channel Count`, guild.channels.size, true)
+		 .addField(`Large?`, guild.large ? "Yes" : "No", true)
+		 .addField(`Server Created At`, guild.createdAt)
+	 bot.channels.get('526573436818948107').send(newserverembed);
+	 //END NEW GUILD ADD
+
+	//	bot.channels.get("430436180723499010").send(`New guild joined: ${guild.name} \nGuild ID: (id: ${guild.id}). \nMembers in Guild ${guild.memberCount})\n Guild Owner: ${guild.owner}`);
+	//start the banishment code
+	if(banished.includes(guild.id)) {
+		try{
+			let banishedReason = guild.channels.filter(c => c.type === "text" && c.permissionsFor(guild.me).has("SEND_MESSAGES")).first()
+			banishedReason.send(`:x: | This guild has been blacklisted, ${bot.user.username} is not allowed to be here which is a shame because I think we could of been friends, anyway :wave:`)    
+		}catch(e){
+		console.log(`I couldn't find a channel to send the message in`)
+		}
+		   setTimeout(async () => {
+		 await guild.leave().catch(err => console.log(err))
+		}, 5000)
+	}
+	//end the baishment code
+	
+	});
+
+bot.on("guildDelete", guild => {
+	console.log(`Left guild ${guild.name} (id: ${guild.id}). This guild had ${guild.memberCount} members!`);
+	//bot.channels.get("430436180723499010").send(`Left guild ${guild.name} \nGuild ID: (id: ${guild.id}). \nGuild had: ${guild.memberCount} members!\nGuild Owner: ${guild.owner}`);
+		 //BEGIN NEW GUILD ADD
+		 let serverSize = guild.memberCount;
+		 let botCount = guild.members.filter(m => m.user.bot).size;
+		 let humanCount = serverSize - botCount;
+		 let verifLevels = ["None", "Low\nmust have verified\nemail on account", "Medium - must be registered on Discord for longer than 5 minutes", "High -  (╯°□°）╯︵ ┻━┻ - must be a member of the server for longer than 10 minutes", "Very High - ┻━┻ミヽ(ಠ益ಠ)ﾉ彡┻━┻ - must have a verified phone number"];
+		
+		 const serverleftembed = new Discord.RichEmbed()
+			 .setColor(`0x008000`)
+			 .setAuthor(`Owner: ${guild.owner.user.tag}`, guild.owner.user.displayAvatarURL)
+			 .setFooter(`Guild Name: ${guild.name} ID: ${guild.id}`, guild.iconURL)
+			 .setThumbnail(guild.iconURL ? guild.iconURL : guild.owner.user.displayAvatarURL)
+			 .setTimestamp()
+			 .setTitle(`Server Left`)
+			 .addField(`Server Name`, guild.name, true)
+			 .addField(`Server ID`, guild.id, true)
+			 .addField(`Server Owner`, guild.owner.user.tag, true)
+			 .addField(`Server Owner ID`, guild.ownerID, true)
+			 .addField(`Server Region`, guild.region, true)
+			 .addField(`Verification Level`, verifLevels[guild.verificationLevel], true)
+			 .addField(`Total Members`, serverSize, true)
+			 .addField(`Total Bots`, botCount, true)
+			 .addField(`Total Humans`, humanCount, true)
+			 .addField(`Emoji Count`, guild.emojis.size, true)
+			 .addField(`Role Count`, guild.roles.size, true)
+			 .addField(`Channel Count`, guild.channels.size, true)
+			 .addField(`Large?`, guild.large ? "Yes" : "No", true)
+			 .addField(`Server Created At`, guild.createdAt)
+		 bot.channels.get('526573436818948107').send(serverleftembed);
+		 //END NEW GUILD ADD
+});
+
   //begin Marks code for stats
+  
   bot.on('guildMemberAdd', member => {
-		member.guild.channels.get('430206024243478528').setName(`?? Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`)
-			member.guild.channels.get('430206266208419851').setName(`?? Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`)
-			member.guild.channels.get('430206301537304576').setName(`? Bots + Members : ${member.guild.members.size}`)
+			if(member.guild.id !== ('389472576235372565')) return;
+			member.guild.channels.get('430206024243478528').setName(`Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`)
+			member.guild.channels.get('430206266208419851').setName(`Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`)
+			member.guild.channels.get('430206301537304576').setName(`Bots + Members : ${member.guild.members.size}`)
 	
 		});
 	
 		bot.on('guildMemberRemove', member => {
-			member.guild.channels.get('430206024243478528').setName(`?? Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`)
-			member.guild.channels.get('430206266208419851').setName(`?? Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`)
-			member.guild.channels.get('430206301537304576').setName(`? Bots + Members : ${member.guild.members.size}`)
+			if(member.guild.id !== ('389472576235372565')) return;
+			member.guild.channels.get('430206024243478528').setName(`Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`)
+			member.guild.channels.get('430206266208419851').setName(`Bot Count : ${member.guild.members.filter(m => m.user.bot).size}`)
+			member.guild.channels.get('430206301537304576').setName(`Bots + Members : ${member.guild.members.size}`)
 			
 			});
 			//end Marks code for stats
 
-	bot.on("guildCreate", guild => {
-				console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-					bot.channels.get("430436180723499010").send(`New guild joined: ${guild.name} \nGuild ID: (id: ${guild.id}). \nMembers in Guild ${guild.memberCount})\n Guild Owner: ${guild.owner}`);
-				});
-			
-	bot.on("guildDelete", guild => {
-				console.log(`Left guild ${guild.name} (id: ${guild.id}). This guild had ${guild.memberCount} members!`);
-				bot.channels.get("430436180723499010").send(`Left guild ${guild.name} \nGuild ID: (id: ${guild.id}). \nGuild had: ${guild.memberCount} members!\nGuild Owner: ${guild.owner}`);
-			  });
+
 
 // Create an event listener for messages
 bot.on('message', async message => {
-
+	if(message.author.bot) return; //stops GreenBot from spamming the prefix when he sees the word "prefix"
+	if(message.member.displayName.startsWith("[AFK]")) {
+		message.reply("Do `g!bk` to turn off AFK mode!").then(m => m.delete(5000));
+	}
 	//users who have the role set below will have their messages deleted
 	const msgBanned = bot.guilds.get("389472576235372565").roles.find("name", "mute"); //only works on Greens main server
 	if(!message.guild) return //if message is not in a guild, ignore
@@ -78,52 +178,104 @@ bot.on('message', async message => {
 	return message.reply("You have been blocked from sending attachments in this server!")
 		}
 	// Attachment Block End
-	if (message.content.toLowerCase() == "green") {
-		message.channel.send(":penguin:");
+	if (message.content == "Master") {
+		var greenO = bot.users.get("188861825100677120").tag
+		message.channel.send(`**${greenO} Is My Master**`);
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`Master`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
 	}
-	
+	if (message.content == "master")  {
+		var greenO = bot.users.get("188861825100677120").tag
+		message.channel.send(`**${greenO} Is My Master**`);
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`master`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+	}
+
 	if (message.content =="g!") {
 		message.channel.send("So.... you want me to run a command... well tell me which one noob, I'm not physic!");
 	}
 
-	if (message.content == "sf") {
-		message.channel.send("Did you want this? mc.slimefantasy.com");
+	if (message.content == "g!twitter") {
+		message.channel.send("https://twitter.com/GreensaPenguin");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`GreensaPenguin's Twitter Link (!twitter)`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)		
 	}
 
 	if (message.content.toLowerCase() =="penguin") {
 		message.channel.send(":penguin:");
 	}
-	
+
 	if (message.content.toLowerCase() =="penguins") {
 		message.channel.send(":penguin::penguin::penguin:");
 	}
 	
-	if (message.content.toLowerCase =="greenbot") {
+	if (message.content.toLowerCase() =="greenbot") {
 		message.channel.send(`${message.author.username} loves GreenBot :green_heart:`);
 	}
 	
-	if (message.content =="monkey") {
-		message.channel.send(`:monkey:`);
-	}
-	
-	if (message.content =="monkeys") {
-		message.channel.send(`:monkey: :monkey: :monkey:`);
-	}
-	
-	if (message.content =="g!retard") {
-		message.channel.send("https://imgur.com/fiCpVCB");
+	if (message.content =="g!noots") {
+		message.channel.send("http://noots.greensapenguin.com/");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`noots`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
 	}
 		
-	if (message.content =="g!monkeyvideo") {
-		message.channel.send("https://www.youtube.com/watch?v=8VnYliTyxjY");
-
+	if (message.content =="g!pingu") {
+		message.channel.send("**Noot Noot**");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`pingu`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
 	}
+	
+	if (message.content =="g!lb") {
+		message.channel.send("http://littlebitch.greensapenguin.com/");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`lb`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+	}
+		
+	if(message.content.includes("cookies")) {
+		message.channel.send("https://tenor.com/YM4e.gif")
+		//bot.channels.get("530923952412033044").send(`${message.author.tag} has used cookies in ${message.guild.name}`)
+	 }
 	
 	if (message.content =="g!penguinvideo") {
 		message.channel.send("**penguins are bae** https://www.youtube.com/watch?v=c7M686pXr6M");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`Penguin Video`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
 	}
+	
+	if (message.content =="g!reee") {
+		message.channel.send("REEEEEEEEEEEE");
+		let commandLog = bot.channels.get('530923952412033044')
+		let command = ("`reee`")
+		let guild = message.guild.name
+		commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+	}
+	
+	// if (message.content =="g!vote") { //wow
+	// 	message.channel.send("**Vote for MarksBot And Elara** \n**MarkBots Link** https://discordbots.org/bot/417143274713776139/vote \n**Elara link** https://discordbots.org/bot/455166272339181589/vote");
+	// 	let commandLog = bot.channels.get('530923952412033044')
+	// 	let command = ("`Penguin Video`")
+	// 	let guild = message.guild.name
+	// 	commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+	// }
 
-	if(message.content === `<@${bot.user.id}>`) return message.channel.send(`My prefix for **[${message.guild.name}]** is \`${botConfig.prefix}\``);
+	// if (message.content.includes("prefix")) {
+	// 	message.channel.send(`I heard the word \`prefix\` My prefix is \`${botConfig.prefix}\` use \`g!help\` to get started`)
+	// 	}
+	if(message.isMentioned(bot.user.id) && message.content.toLowerCase().includes('prefix')) return message.channel.send(`I heard the word \`prefix\` My prefix is \`${botConfig.prefix}\` use \`g!help\` to get started`);
 	if(message.author.bot) return; //stops the bot from respoinding to itself 
 	if(message.channel.type === "dm") return; //make the bot not repsond to commands in DM because why....
 
@@ -153,12 +305,34 @@ bot.on('message', async message => {
 	if(cmd) cmd.run(bot, message, args);
 });
 
-
-
 	// Log our bot in, keep the token save and don't post it on your server lol 
 	// this reads it from a file anyway so if anyone was to see you in this file they wont see your secret token
 bot.login(botConfig.GreenLogin);
-//Error handling
-bot.on("error", (e) => console.error(e));
-bot.on("warn", (e) => console.warn(e));
+
+//Error handling (Simplified)
+process.on('unhandledRejection', error => {   
+	console.log(`ERROR:\n${error.stack}`)
+	  let gbe = new Discord.RichEmbed()
+	  .setColor(`#FF0000`)
+	  .setTimestamp()
+	  .setDescription(error.stack)
+	  .setTitle(`Unhandled Rejection`)
+	  .setAuthor(bot.user.tag, bot.user.displayAvatarURL)
+	  bot.channels.get('567115961379979269').send(gbe)
+	}).on('uncaughtException', error => {
+		let gbe = new Discord.RichEmbed()
+		.setDescription(error.stack)
+		.setTitle(`Uncaught Exception`)
+		.setAuthor(bot.user.tag, bot.user.displayAvatarURL)
+		.setColor(`#FF0000`)
+		.setTimestamp()
+		bot.channels.get('567115961379979269').send(gbe)
+	});
+
+//bot.on("error", (e) => console.error(e));
+//bot.on("warn", (e) => console.warn(e));
 //bot.on("debug", (e) => console.info(e)); //remove the first // if you really want to see debug info
+
+//Last Update by Mark 17th Feb 2019
+// Last Change: Error handling system to post to channel
+// Last Change : Sexy join/leave logs & guild blacklist system (/classes/noGreenBot4u.json)
