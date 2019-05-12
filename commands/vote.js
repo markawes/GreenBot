b5792b03c5768752179af6a8c5030a3339c8b731
GreensaPenguin <50413041+GreensaPenguin@users.noreply.github.com>
2019-05-12 18:59:14 -0400

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -10,6 +10,7 @@ let ev = new Discord.RichEmbed()
 .setThumbnail(message.author.displayAvatarURL)
 .addField("Elara", "https://discordbots.org/bot/455166272339181589/vote")
 .addField(`MarksBot`, `https://discordbots.org/bot/417143274713776139/vote`)
+.addField(`GreenBot`, `https://discordbots.org/bot/432267856869064704/vote`)
 message.channel.send(ev)
 }
 
