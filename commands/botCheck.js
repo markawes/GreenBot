e93e6fb968e913960624b72cd41580b1ee3fea62
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-05-15 16:29:14 -0700

    Add "ignore" for bot list server ex: DBL

--- commands/botCheck.js
+++ commands/botCheck.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js")
-let users = ['188861825100677120', '272442568275525634', '288450828837322764'];
+let users = ['188861825100677120', '272442568275525634', '288450828837322764']; // Users who can do the "g!botcheck" command
+let ignore = ["264445053596991498"]; // Ignore servers like DBL.. 
 module.exports.run = async (bot, message) => {
       if(!users.includes(message.author.id)) return message.react("❌")
       let servers = [];
-        await bot.guilds.filter(s => s.members.filter(m => m.user.bot).size >= 50).map(c => servers.push(`**Server: **${c.name} (${c.id})\n**Bot Count: **${c.members.filter(m => m.user.bot).size}\n**Human Count: **${c.members.filter(m => !m.user.bot).size}`))
+        await bot.guilds.filter(s => s.members.filter(m => m.user.bot).size >= 50 && !ignore.includes(s.id)).map(c => servers.push(`**Server: **${c.name} (${c.id})\n**Bot Count: **${c.members.filter(m => m.user.bot).size}\n**Human Count: **${c.members.filter(m => !m.user.bot).size}`))
         let e = new Discord.RichEmbed()
         .setColor("#008000")
         if(servers.length !== 0){
