75c3849d9553b43e454c0e1585b25ce953d6825b
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-17 02:15:00 +0100

    Update !.js

--- commands/!.js
+++ commands/!.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message) => {
-  message.channel.send(`:computer: : ${bot.guilds.size}\n:busts_in_silhouette: : ${bot.users.filter(h => !h.bot).size}\n:robot: : ${bot.users.filter(b => b.bot).size}`)
+  message.channel.send(`Guilds : ${bot.guilds.size}\nHoomans : ${bot.users.filter(h => !h.bot).size}\nBots : ${bot.users.filter(b => b.bot).size}`)
 }
 module.exports.help = {
     name:"!"
