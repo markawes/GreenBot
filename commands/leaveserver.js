c29af4900ae79c021bd367ef5e37b0a607225ed4
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-05-16 19:23:35 -0700

    Update leaveserver.js

--- commands/leaveserver.js
+++ commands/leaveserver.js
@@ -8,7 +8,17 @@ module.exports.run = async (bot, message, args) => {
       if(!guild) return message.channel.send(`Well.. uh, what is the valid server id?..`);
       let msg = await message.channel.send(`Trying to leave.. ${guild.name}, one moment please..`);
       try{
-      await guild.leave().then(async () => {
+const owner = bot.users.get('188861825100677120')
+ const embed = new Discord.RichEmbed()
+ .setColor("#ff0000")
+ .setAuthor(`${bot.user.username} has left this server`, bot.user.displayAvatarURL)
+ .setTitle(`The powers above have requested that I ${bot.user.username} leave this server, for more information please join the support server`)
+ .setThumbnail(bot.user.displayAvatarURL)
+ .addField(`${bot.user.username} Support Server`, "https://discord.gg/YGMcEQ3")
+ .addField(`BOT Owner`, `${owner.tag}`) 
+ let channel = await guild.channels.filter(c => c.type === "text" && c.permissionsFor(guild.me).has(["EMBED_LINKS", "SEND_MESSAGES"])).first()
+ if(channel){channel.send(embed)}     
+ await guild.leave().then(async () => {
       return msg.edit(`Successfully left.. ${guild.name}`);
       })
 }catch(e){
