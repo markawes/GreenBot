8f5245e75856a972fdaff2e0a0ecea290e1f1f4f
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-05-18 03:34:45 +0100

    Update !.js

--- commands/!.js
+++ commands/!.js
@@ -1,6 +1,14 @@
+const Discord = require("discord.js")
 module.exports.run = async (bot, message) => {
-  message.channel.send(`Guilds : ${bot.guilds.size}\nHoomans : ${bot.users.filter(h => !h.bot).size}\nBots : ${bot.users.filter(b => b.bot).size}`)
+  const e = new Discord.RichEmbed()
+  .setAuthor(`${bot.user.username}`)
+  .setTitle(`Some Stats`)
+  .setDescription(`Guilds : ${bot.guilds.size}\nHuman Users : ${bot.users.filter(h => !h.bot).size}\nBots : ${bot.users.filter(b => b.bot).size}`)
+  .setThumbnail("https://greenbot.mwserver.co/images/coolBlob.gif")
+  .setFooter("Shocking markawes and SUPERCHIEFYT since 2019")
+  message.channel.send(e)
 }
+
 module.exports.help = {
     name:"!"
 }
