04aa74197496cd50e4f81fbba402c21bdb6f4954
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-05-27 17:20:35 -0700

    Update checkWebhooks.js

--- commands/checkWebhooks.js
+++ commands/checkWebhooks.js
@@ -1,4 +1,5 @@
 module.exports.run = (bot, message) => {
+  if(!message.guild.me.hasPermission("MANAGE_WEBHOOKS")) return message.channel.send(`I need "Manage Webhooks" permission to view the webhooks in the server!`)
   message.guild.fetchWebhooks()
     .then(webhooks => message.reply(`I found ${webhooks.size} webhooks in this guild`)).catch(console.error);
 	}
