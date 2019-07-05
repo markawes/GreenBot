354d0dbb450843c3cf8f7a3eee470478f3f55760
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:49:42 -0700

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,8 +1,4 @@
 module.exports.run = async (bot, message, args) => {
-  let commandLog = bot.channels.get('530923952412033044')
-  let command = ("`afk`")
-  let guild = message.guild.name
-  commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     try {
         if (!args[0]) return message.channel.send("Please tell everyone why you are going AFK")
         if(message.member.displayName.startsWith("[AFK]")){
