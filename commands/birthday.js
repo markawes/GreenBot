f4452b023a47154d2b62ff0f6fbf819a7e6f9fb4
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:48:51 -0700

    Update birthday.js

--- commands/birthday.js
+++ commands/birthday.js
@@ -1,8 +1,4 @@
 module.exports.run = async (bot, message, args) => {
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`birthday`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     let BirthdayMsg = await message.channel.send("Birthday, what, buying a card");
     let target = message.mentions.users.first() || message.author;
     message.channel.send(`Happy birthday ${target}`)
