2e50fb9f242c1a671a0291a2111e4e2a29ac9201
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:49:32 -0700

    Update afkBack.js

--- commands/afkBack.js
+++ commands/afkBack.js
@@ -1,8 +1,4 @@
 module.exports.run = async (bot, message, args) => {
-  let commandLog = bot.channels.get('530923952412033044')
-  let command = ("`bk` (no longer afk)")
-  let guild = message.guild.name
-  commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
    if(!message.member.displayName.startsWith("[AFK]")){
      message.reply("You are not set AFK.")
    }
