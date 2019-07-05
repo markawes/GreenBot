30d4799aff76461b177981001c2a12b1c6d83b2b
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:46:30 -0700

    Update botinserver.js

--- commands/botinserver.js
+++ commands/botinserver.js
@@ -1,8 +1,4 @@
 module.exports.run = async (bot, message, args) => {
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`bots`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     var member = message.guild.members.filter(m => m.user.bot).size
     message.channel.send(`Here at ${guild} we have **${member}** bots`)
 }
