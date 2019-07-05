e6e7df36a5d5c1d79362dedf86bf5ca38815be08
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:48:36 -0700

    Update botInviteLink.js

--- commands/botInviteLink.js
+++ commands/botInviteLink.js
@@ -1,12 +1,9 @@
 const Discord = require("discord.js")
 const botSettings = require("../botConfig.js")
 module.exports.run = (bot, message, args) => {
-    if(!botSettings.ownerDev.includes(message.author.id)){
-        return message.reply("This command is locked to the bot owner!"); //lock command to Green only.
-        }
    const embed = new Discord.RichEmbed()
    .setTitle("Invite me to your server")
-   .setDescription("https://discordapp.com/oauth2/authorize?client_id=432267856869064704&scope=bot&permissions=2080767222")
+   .setDescription(`https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2080767222`)
    .setTimestamp();
 
    message.channel.send(embed)
