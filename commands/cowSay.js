00f3b6a86e75b0023fd758a89e27408c8d93621f
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:44:41 -0700

    Update cowSay.js

--- commands/cowSay.js
+++ commands/cowSay.js
@@ -1,10 +1,6 @@
 const cowsays = require("cowsay");
 
 exports.run = async (bot, message) => {
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`cowsay`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
    // message.delete()
     var args = message.content.split(' ').slice(1).join(' ');
     if (!args) {
