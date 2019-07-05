60441df076e344fa28721ad8fde4e3a29c465c23
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:43:06 -0700

    Update googleit.js

--- commands/googleit.js
+++ commands/googleit.js
@@ -1,8 +1,4 @@
 module.exports.run = (bot, message) => {
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`googleit`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     message.channel.send("https://i.imgur.com/X20kba7.gif")
 }
 
