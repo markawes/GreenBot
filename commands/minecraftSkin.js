b56c64d491c466716290e3715d2eb92ab3ad1ffc
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 20:03:26 -0700

    Update minecraftSkin.js

--- commands/minecraftSkin.js
+++ commands/minecraftSkin.js
@@ -1,13 +1,12 @@
 const Discord = require("discord.js")
 module.exports.run = (bot, message, args) => {
-    const args = message.content.split(" ").slice(1).join("")
-    if (!args) return message.reply("**Type a players name**");
-    const image = (`https://minotar.net/armor/body/${args}`);
+    if (!args.join(" ")) return message.reply("**Type a players name**");
+    const image = (`https://minotar.net/armor/body/${args.join(" ")}`);
 
     const mcEmbed = new Discord.RichEmbed()
     .setTitle("Minecraft Player Skin")
     .setFooter(`Requested by ${message.author.username}`)
-    .setDescription(`You asked us to search for ${args}`)
+    .setDescription(`You asked us to search for ${args.join(" ")}`)
     .setColor(0x2caecb)
     .addField("We found", "the following image")
     .setImage(`${image}`)
