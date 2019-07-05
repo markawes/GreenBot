07321b2d46740e2e9fc042c42428de2b48bc4655
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:46:18 -0700

    Update checkBans.js

--- commands/checkBans.js
+++ commands/checkBans.js
@@ -1,9 +1,6 @@
 module.exports.run = (bot, message) => {
 	if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`I need "Ban Members" permission to view the bans in the server!`)
-    let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`checkbans`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+ 
     message.guild.fetchBans()
 	.then(bans => message.channel.send(`This guild has ${bans.size} banned users`))
 	.catch(console.error);
