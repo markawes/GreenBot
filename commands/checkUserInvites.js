a1a3528e4a7ab8e3e2efce020f6648da9325f3ae
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:45:15 -0700

    Update checkUserInvites.js

--- commands/checkUserInvites.js
+++ commands/checkUserInvites.js
@@ -1,9 +1,6 @@
 module.exports.run = (bot, message) => {
 	if(!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send(`I need "Manage Server" permission to view the invites in the server!`)
-  let commandLog = bot.channels.get('530923952412033044')
-    let command = ("`checkinvites`")
-    let guild = message.guild.name
-    commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
+
     message.guild.fetchInvites()
     .then(invites => message.reply(`I found ${invites.size} invites codes in this guild`))
     .catch(console.error)
