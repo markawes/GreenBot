5d948e4f516de9274584f56253fa1a68bf291fc0
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:31:30 -0700

    Switching it to auto command logging..

--- commands/urbanDictionary.js
+++ commands/urbanDictionary.js
@@ -3,10 +3,6 @@ const urban = require("relevant-urban")
 
 module.exports.run = async (bot, message, args) => {
    if(!message.channel.nsfw) return message.channel.send("Sorry, this command can only be ran in `NSFW`channels")
-  let commandLog = bot.channels.get('530923952412033044')
-  let command = ("`urban`")
-  let guild = message.guild.name
-  commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     if (!args[0]) return message.channel.send("What urban term do you want?");
 
     let res = await urban(args.join(' ')).catch(e => {
