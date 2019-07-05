b34dc16d64556ee027caa8f959883171d0ee23e9
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-04 19:32:43 -0700

    Switching it to auto command logging..

--- commands/sleep.js
+++ commands/sleep.js
@@ -4,10 +4,6 @@ module.exports.run = (bot, message, [interval, period]) => {
     if(!config.ownerDev.includes(message.author.id)){
         return message.channel.send("This command is locked to the bot owner and the bot developer!");
         } else {
-            let commandLog = bot.channels.get('530923952412033044')
-            let command = ("`sleep`")
-            let guild = message.guild.name
-            commandLog.send(`${message.author.tag} has used ${command} in ${guild}`)
     const timer = moment.duration(parseInt(interval, 10), period).asMilliseconds();
   console.log(timer);
   message.channel.send(`${bot.user} is going to sleep for ${timer} milliseconds :wave:`)
