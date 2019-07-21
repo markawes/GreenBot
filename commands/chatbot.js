f37e6b93606aa61d61334a6aa331d467f26dbaf5
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-20 23:42:18 -0700

    Update chatbot.js

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -1,10 +1,14 @@
 const fetch = require("snekfetch")
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.reply("Please talk to me!")
+    try{
+     if(!args[0]) return message.reply("Please talk to me!")
     message.channel.startTyping(true)
     let {body} = await new fetch("GET", `https://some-random-api.ml/chatbot/?message=${args.join(" ")}`)
     message.channel.send(body.response)
 await message.channel.stopTyping(true)
+    }catch(e){
+    message.channel.send(`Error while running the command:\n${e}`)
+    }
 }
 
 module.exports.help = {
