231c4e8ca027a5a252c190a2e4dff2d190d3dbec
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-20 23:20:50 -0700

    Update checkBreach.js

--- commands/checkBreach.js
+++ commands/checkBreach.js
@@ -1,14 +1,9 @@
 const CheckDB = require("snekfetch");
 exports.run = async (bot, message, args) => {
     if(!args[0]) return message.reply("Please provide an e-mail address to check");
-    await message.delete(300);
-    let {
-        body
-    } = await CheckDB
-        .get(`https://haveibeenpwned.com/api/v2/breachedaccount/${args[0]}`)
-        .catch(err => {
-            message.reply(`Upon checking the database no results were found for \`\`${args[0]}\`\` this is good news! :thumbsup:`)
-        });
+    await message.delete(300).catch(o_O => {});
+    let {body} = await CheckDB.get(`https://haveibeenpwned.com/api/v2/breachedaccount/${args[0]}`);
+    if(!body || body === null || body === undefined) return message.reply(`Upon checking the database no results were found for \`\`${args[0]}\`\` this is good news! :thumbsup:`)
 
     let out = ` Unfortunatly breaches have been found for: **${args[0]}**, we suggest you check this and take measures to secure your accounts\n`;
     let po = 0;
@@ -16,8 +11,8 @@ exports.run = async (bot, message, args) => {
         po++;
         out += `\n${po}.   ${i.Name}   breached on:   ${i.BreachDate}`
     })
-    if(out.length > 2000) return message.reply("Too many results, please check https://haveibeenpwned.com/")
-    message.reply(out);
+    if(out.length > 2000) return message.reply("Too many results, please check https://haveibeenpwned.com/").catch(o_O => {})
+    message.reply(out).catch(o_O => {})
    
 };
 
