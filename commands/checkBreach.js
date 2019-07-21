932d9029a55b56ac0ad3d7fc3988943a4b66208b
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-07-21 14:23:20 +0100

    Update checkBreach.js

--- commands/checkBreach.js
+++ commands/checkBreach.js
@@ -1,9 +1,9 @@
 const CheckDB = require("snekfetch");
 exports.run = async (bot, message, args) => {
-    return message.reply(`Sorry.. this command is currently borked.. and has been disabled by one of the bot devs.`)
+   // return message.reply(`Sorry.. this command is currently borked.. and has been disabled by one of the bot devs.`)
     if(!args[0]) return message.reply("Please provide an e-mail address to check");
     await message.delete(300).catch(o_O => {});
-    let {body} = await CheckDB.get(`https://haveibeenpwned.com/api/v2/breachedaccount/${args[0]}`);
+    let {body} = await CheckDB.get(`https://haveibeenpwned.com/api/v2/breachedaccount/${args[0]}`).set("hibp-api-key", process.env.hibp)
     if(!body || body === null || body === undefined) return message.reply(`Upon checking the database no results were found for \`\`${args[0]}\`\` this is good news! :thumbsup:`)
 
     let out = ` Unfortunatly breaches have been found for: **${args[0]}**, we suggest you check this and take measures to secure your accounts\n`;
@@ -20,3 +20,4 @@ exports.run = async (bot, message, args) => {
 module.exports.help = {
     name:"cb"
 }
+//Temp fix
