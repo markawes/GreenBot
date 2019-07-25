d4ba19c71182e9ad521e1aa5dc0cc2a0b5efa9ec
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-25 13:38:17 -0700

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -2,8 +2,8 @@ const ms = require('ms'), locks = new Set(), valid = ["unlock", "lift", "un"];
 module.exports.run = async (bot, message, args) => {
   try{
   if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`No can do.. you don't have \`Manage Server\` permission`)
-  if(locks.has(message.channel.id) && !valid.includes(args[0].toLowerCase())) return message.channel.send(`This channel is already in lockdown!`)
-  if(locks.has(message.channel.id) && valid.includes(args[0].toLowerCase())){
+  if(locks.has(message.channel.id) && !valid.includes(args[0] ? args[0].toLowerCase() : "No")) return message.channel.send(`This channel is already in lockdown!`)
+  if(locks.has(message.channel.id) && valid.includes(args[0] ? args[0].toLowerCase() : "No")){
   await locks.delete(message.channel.id);
   message.channel.overwritePermissions(message.guild.id, {SEND_MESSAGES: null})
   return message.channel.send(`Channel: Unlocked`);
