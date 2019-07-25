9d0a7a11dad7b9dde1584f50c2b75efff99d8526
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-25 13:33:26 -0700

    Change the lockdown command.. ig

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,45 +1,21 @@
-const ms = require('ms');
-const botSettings = require("../botConfig.js")
-exports.run = (bot, message, args) => {
-    if(!botSettings.ownerDev.includes(message.author.id)){
-        return message.channel.send(":x: Nice try, but you do not have permission to authorise a lockdown!");
-        } 
-    if (!bot.lockit) bot.lockit = [];
-    let time = args.join(' ');
-    let validUnlocks = ['release', 'unlock', 'pizza', 'cheese']
-
-
-  
-    if (!time) return message.reply('you must set a duration for the lockdown in either hours, minutes or seconds.');
-
-    if (validUnlocks.includes(time)) {
-        message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: null
-        }).then(() => {
-            message.channel.send('**Lockdown lifted.**');
-            clearTimeout(bot.lockit[message.channel.id]);
-            delete bot.lockit[message.channel.id];
-        }).catch(error => {
-            console.log(error);
-        });
-    } else {
-        message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: false
-        }).then(() => {
-            message.channel.send(`**Channel locked** for ${ms(ms(time), { long:true })}.`).then(() => {
-
-                bot.lockit[message.channel.id] = setTimeout(() => {
-                    message.channel.overwritePermissions(message.guild.id, {
-                        SEND_MESSAGES: null
-                    }).then(message.channel.send('**Lockdown lifted.**')).catch(console.error);
-                    delete bot.lockit[message.channel.id];
-                }, ms(time));
-
-            }).catch(error => {
-                console.log(error);
-            });
-        });
-    }
+const ms = require('ms'), locks = new Set(), valid = ["unlock", "lift", "un"];
+module.exports.run = async (bot, message, args) => {
+  try{
+  if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`No can do.. you don't have \`Manage Server\` permission`)
+  if(locks.has(message.channel.id) && !valid.includes(args[0].toLowerCase())) return message.channel.send(`This channel is already in lockdown!`)
+  if(locks.has(message.channel.id) && valid.includes(args[0].toLowerCase())){
+  await locks.delete(message.channel.id);
+  message.channel.overwritePermissions(message.guild.id, {SEND_MESSAGES: null})
+  return message.channel.send(`Channel: Unlocked`);
+  }else{
+  await locks.add(message.channel.id);
+  message.channel.overwritePermissions(message.guild.id, {SEND_MESSAGES: false});
+  return message.channel.send(`Channel: Locked\nUse: "g!lockdown unlock" to unlock the channel!`);
+  }
+  }catch(error){
+  message.channel.send(`Error:\n${error}`);
+  bot.error(bot, "Command `lockdown` Error", error.stack)
+  }
 };
 
 module.exports.help = {
