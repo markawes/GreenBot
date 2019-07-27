c5d2503a01c2993def3f5b75214bf34eb1175fcb
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-07-27 15:06:27 -0700

    Update botCheck.js

--- commands/botCheck.js
+++ commands/botCheck.js
@@ -4,7 +4,7 @@ let ignore = ["264445053596991498"]; // Ignore servers like DBL..
 module.exports.run = async (bot, message) => {
       if(!users.includes(message.author.id)) return message.react("❌")
       let servers = [];
-        await bot.guilds.filter(s => s.members.filter(m => m.user.bot).size >= 50 && !ignore.includes(s.id)).map(c => servers.push(`**Server: **${c.name} (${c.id})\n**Bot Count: **${c.members.filter(m => m.user.bot).size}\n**Human Count: **${c.members.filter(m => !m.user.bot).size}`))
+        await bot.guilds.filter(s => Math.round(((s.members.filter(c => c.user.bot).size / s.memberCount) * 100).toFixed(2)) >= "70" && !ignore.includes(s.id)).map(c => servers.push(`**Server: **${c.name} (${c.id})\n**Bot Count: **${c.members.filter(m => m.user.bot).size}\n**Human Count: **${c.members.filter(m => !m.user.bot).size}`))
         let e = new Discord.RichEmbed()
         .setColor("#008000")
         if(servers.length !== 0){
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message) => {
         return message.channel.send(e)
         }else{
         e.setTitle(`Servers: [0]`)
-        .setDescription(`No servers have over 50 bots.`)
+        .setDescription(`No servers have 70% more bots than humans`)
         return message.channel.send(e)
         }
 }
