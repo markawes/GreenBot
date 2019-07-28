cdfaa40f67b7edb3d32537548d541be8f3350732
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-07-29 00:39:12 +0100

    Update de.js

--- commands/de.js
+++ commands/de.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js")
 const superagent = require("superagent")
 const DEmojiJS = require("demojijs")
 module.exports.run = async (bot, message, args) => {
-if(!message.channel.nsfw) return message.channel.send("Please run this command in an NSFW channel as it could show naughty emotes!")
-DEmojiJS.randomEmoji().then(async Emoji => {
+//if(!message.channel.nsfw) return message.channel.send("Please run this command in an NSFW channel as it could show naughty emotes!")
+if(message.guild.id === "264445053596991498") return;
+    DEmojiJS.randomEmoji().then(async Emoji => {
 let ei = new Discord.RichEmbed().setAuthor(message.author.username, message.author.displayAvatarURL).setColor(message.member.displayColor)
 .setAuthor(`Uploaded by : [${Emoji.submitted_by}]`)
 .setTitle(`Emoji Name : [${Emoji.title}]`)
