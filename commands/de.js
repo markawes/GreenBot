6fb96f0f7c15bfd5c8c2bd9b0bc45a0592e20dea
Mark Wesley <49736096+markawes@users.noreply.github.com>
2019-07-29 00:14:33 +0100

    Create de.js

---
+++ commands/de.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+const superagent = require("superagent")
+const config = require("../../config.json");
+const DEmojiJS = require("demojijs")
+module.exports.run = async (bot, message, args) => {
+if(!message.channel.nsfw) return message.channel.send("Please run this command in an NSFW channel as it could show naughty emotes!")
+DEmojiJS.randomEmoji().then(async Emoji => {
+let ei = new Discord.RichEmbed().setAuthor(message.author.username, message.author.displayAvatarURL).setColor(message.member.displayColor)
+.setAuthor(`Uploaded by : [${Emoji.submitted_by}]`)
+.setTitle(`Emoji Name : [${Emoji.title}]`)
+.setImage(Emoji.image)
+.setFooter("Random emoji from discordemoji.com")
+    message.channel.send(ei);
+ }).catch(console.error);
+}
+
+module.exports.help = {
+    name:"de"
+}
+
