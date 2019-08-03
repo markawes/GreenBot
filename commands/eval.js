3883dfc33975dc2d6d732bc232ae3829eb19eb8f
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-08-02 18:01:28 -0700

    Update Eval... :p

--- commands/eval.js
+++ commands/eval.js
@@ -4,27 +4,24 @@ const Discord = module.require("discord.js");
 
 //const moment = require("moment")
 module.exports.run = async (bot, message, args) => { 
-   
-      if(!bot.odevs.includes(message.author.id)) return message.react("393617452992430080");
-      //message.delete();
-const arg = message.content.split(" ").slice(1);
-const clean = text => {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+   if(!bot.odevs.includes(message.author.id)) return message.react("393617452992430080");
+   const clean = text => {
+      if(typeof(text) === "string") return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+      else return text;
   }
           try {
-            const code = arg.join(" ");
+            const code = args.join(" ");
             let evaled = eval(code);
-      
             if (typeof evaled !== "string")
               evaled = require("util").inspect(evaled);
-      
-            message.channel.send(clean(evaled), {code:"xl"});
+            evaled.replace(new RegExp(botConfig.token, "g"), "-- Discord Bot Token --").replace(new RegExp(bot.token, "g"), "-- Discord Bot Token --").replace(new RegExp(process.env.token, "g"), "-- Discord Bot Token --").replace(new RegExp(process.env.dblkey, "g"), "-- DBL KEY --")
+            message.channel.send({embed: {description: `\`\`\`js\n${clean(evaled)}\`\`\``, title: "Result", color: 0x008000, timestamp: new Date()}});
           } catch (err) {
-            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-            
+            if(message.channel.permissionsFor(message.guild.me).has(["READ_MESSAGES", "SEND_MESSAGES", "EMBED_LINKS"])){
+           message.channel.send({embed: {description: `\`\`\`js\n${clean(err.stack)}\`\`\``, title: "Result - Error", color: 0xFF0000, timestamp: new Date()}});
+            }else{
+               message.author.send(`YOO! I don't have any permissions to talk in ${message.channel} *Sad GreenBot noise*`).catch(o_O => {})
+            }
           }
         };	
 
