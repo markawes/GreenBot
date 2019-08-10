10a8406ad735301b32f98fd5979310c0fef30294
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-08-10 16:30:23 -0700

    Updated...

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,65 +1,19 @@
 const Discord = require("discord.js")
-//const pokemonGif = require('pokemon-gif');
+const pokemonGif = require('pokemon-gif');
 
 module.exports.run = (bot, message, args) => {
-let pokedexNumberToName = require('../node_modules/pokemon-gif/lib/pokedex-number-to-name');
-let pokemonNameToNumber = require('../node_modules/pokemon-gif/lib/pokemon-name-to-number');
-
-let baseUrl = 'http://www.pokestadium.com/sprites/xy/';
-let extension = '.gif';
-
-function getGifByPokedexNumber(pokedexNumber) {
-  if (pokedexNumberToName.hasOwnProperty(pokedexNumber)) {
-    let pokemonUrlName = pokedexNumberToName[pokedexNumber]
-      .toString()
-      .replace(/\./g,'')
-      .replace(/'/g,'')
-      .replace(/\s/g, "-");
-
-    return baseUrl + pokemonUrlName + extension;
-  } else {
-    message.channel.send('Invalid pokedex number ' +  pokedexNumber); return;
-  }
-}
-
-function getGifByPokemonName(pokemonName) {
-  let lowercasePokemonName = pokemonName.toLowerCase();
-
-  if (pokemonNameToNumber.hasOwnProperty(lowercasePokemonName)) {
-    let pokemonUrlName = lowercasePokemonName
-      .replace(/\./g,'')
-      .replace(/'/g,'')
-      .replace(/\s/g, "-");
-
-    return baseUrl + pokemonUrlName + extension;
-  } else {
-    message.channel.send('Sorry, Pokestadium couldn\'t find "' + pokemonName + '"'); return;
-  }
-}
-
-function pokemonGif(identifier) {
-  if (typeof identifier === 'string') {
-    return getGifByPokemonName(identifier);
-  } else if (typeof identifier === 'number') {
-    return getGifByPokedexNumber(identifier);
-  } else {
-    message.channel.send('pokemonGif input must be type string or number'); return;
-  }
-}
-
-module.exports = pokemonGif;
-    let image = args.slice(0).join(" ")
-    if (!args[0]) { return message.reply("Specify a Pokémon please!") } else {
-    message.channel.send({ embed: new Discord.RichEmbed().setTitle(`Here is ${args}`).setColor("#00ff00").setTimestamp().setImage(pokemonGif(image)).setFooter("Image by Pokestadium") })
-    //message.channel.send(pokemonGif(image))
-
-//     const embed = new Discord.RichEmbed()
-//   .setTitle(`Here is ${args}`)
-// .setImage(`${pokemonGif(image)}`)
-// .setFooter(`Image from Pokestadium`)
-//   .setColor(0x00ff33)
-//   message.channel.send({embed})
-}
+try{
+        let poke = await pokemonGif(args.join(' ') || Math.floor(Math.random() * 721) + 1)
+        let embed = new Discord.RichEmbed()
+        .setImage(poke)
+        .setTitle(`Image doesn't load? click here`)
+        .setURL(poke)
+        .setColor(0xFF0000)
+        .setDescription(`Here is your **${args.join(' ') || "Random"}** pokemon`)
+        message.channel.send(embed)
+        }catch(err){
+            return message.channel.send({embed: {title: `No pokemon found`, color: 0xFF0000}})
+ }
 }
 module.exports.help = {
     name: "pokemon"
