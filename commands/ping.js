c663d2e6aed65cfa023e764bf490ef32de1ab2fb
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-08-09 23:04:09 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,7 @@ module.exports.run = async (client, message, args) => {
   let m = await message.channel.send({embed: {title: "One moment please.", color: 0xFF000, author: {name: client.user.tag, icon_url: client.user.displayAvatarURL}}});
         m.edit({embed: {
             color: 0xFF000,
-            author: {
-                name: client.user.tag,
-                icon_url: client.user.displayAvatarURL
-            },
-            title: `<:Greenbot:526224686560968715> Status <:Greenbot:526224686560968715>`,
+            title: `<:Greenbot:526224686560968715> ${client.user.username} Status <:Greenbot:526224686560968715>`,
             fields: [
                 {
                     name: `Latency`,
