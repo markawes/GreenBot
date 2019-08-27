8e66db19912ac9381e05f1b32e4513ec3a771b97
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-08-27 06:48:26 -0700

    Update chatbot.js

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -1,13 +1,19 @@
-const fetch = require("snekfetch")
+const {get} = require("superagent")
 module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send({embed: {title: "Thinking.... ", color: 0xFF000}});
     try{
-     if(!args[0]) return message.reply("Please talk to me!")
+     if(!args[0]) return msg.edit({embed: {title: "Well.. give me something to think about!", color: 0xFF0000}});
     message.channel.startTyping(true)
-    let {body} = await new fetch("GET", `https://some-random-api.ml/chatbot/?message=${args.join(" ")}`)
-    message.channel.send(body.response)
-await message.channel.stopTyping(true)
+    let {body} = await get(`https://some-random-api.ml/chatbot/?message=${args.join(" ")}`)
+    if(body){
+    msg.edit({embed: {title: "<:Greenbot:526224686560968715> Chat Bot <:Greenbot:526224686560968715>", description: body.response, color: 0xFF000}})
+    await message.channel.stopTyping(true)
+    }else{
+    await message.channel.stopTyping(true)
+    msg.edit({embed: {title: `No response, ***Sad ${bot.user.username} noise***`, color: 0xFF0000}})
+    }
     }catch(e){
-    message.channel.send(`Error while running the command:\n${e}`)
+    msg.edit({embed: {title: "Ouch.. that got me thinking to hard.. I got a error", description: e.message, color: 0xFF0000}})
     }
 }
 
